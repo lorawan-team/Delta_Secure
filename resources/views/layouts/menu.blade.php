8528fbaccb7852b4d33cba211fffd8e93f276bee
Joery Zegers <git@jzegers.nl>
2017-01-09 10:50:15 +0100

    add dashboard & user man links

---
+++ resources/views/layouts/menu.blade.php
@@ -0,0 +1,2 @@
+<li><a class="nav-container" href="{!! url('home') !!}"><i class="fa fa-desktop"></i> &nbspDashboard</a></li>
+<li><a class="nav-container" href="{!! url('management') !!}"><i class="fa fa-database"></i> &nbspUser Management</a></li>
