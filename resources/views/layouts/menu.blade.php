369d5f6ebe7ce808dd2bd55b1114bd3f8a5ad0cd
Sven <Svenddv@gmail.com>
2017-01-12 14:03:18 +0100

    menu fixes

--- resources/views/layouts/menu.blade.php
+++ resources/views/layouts/menu.blade.php
@@ -1,4 +1,2 @@
-<li><a class="nav-container" href="{{ url('admin') }}"><i class="fa fa-desktop"></i> &nbspDashboard</a></li>
-<!-- <li><a class="nav-container" ui-sref="user"><i class="fa fa-database"></i> &nbspUser Management</a></li> -->
-
-<ui-view>
+<li><a class="nav-container" href="{{ url('home') }}"><i class="fa fa-desktop"></i> &nbspDashboard</a></li>
+<li><a class="nav-container" href="{{ url('admin') }}"><i class="fa fa-database"></i> &nbspAdmin Panel</a></li>
